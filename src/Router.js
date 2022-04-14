4370003210536268769cf13258f421514e709626
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-04-14 11:36:53 -0400

    first commit + Router component

---
+++ src/Router.js
@@ -0,0 +1,28 @@
+/* 
+    Write code for React Router here
+Import the Home and About components from 
+the component folder. Assign the default
+path "/" to the Home component and "/about"
+to the About component.
+
+Remember to export the component at the end
+*/
+
+import React from 'react'
+import { Switch, Route } from 'react-router'
+// Write component imports here //
+import Home from './components/Home.js'
+import About from './components/About.js'
+
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path = "/about" component={About} />
+        </Switch>
+    )
+}
+
+
+export default Router
+
