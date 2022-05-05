8428a765e537d785ee9d5c5a37a4c499c467ed07
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-05-04 22:17:11 -0400

    MUI integrated successfully + complete assignment

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import App from './App.js'
-import Home from './Home.js'
+/* 
+    Write code for React Router here
+Import the Home and About components from 
+the component folder. Assign the default
+path "/" to the Home component and "/about"
+to the About component.
 
+Remember to export the component at the end
+*/
+
+import React from 'react'
+import { Switch, Route } from 'react-router'
+// Write component imports here //
+import Home from './components/Home.js'
+import About from './components/About.js'
+import Car from './components/Car.js'
 
 const Router = () => {
     return (
-        <Routes>
-            <Route path="/app" component={App} />
-            
-            <Route path="/home" component={Home} />
-           
-        </Routes>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/car/:id" component={Car} />
+        </Switch>
     )
 }
 
+
 export default Router
+
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import cars from '../cars.json'
+import Card from './Card/Card'
+import Chip from '@material-ui/core/Chip';
+// import Stack from '@mui/material/Stack';
+
 // import material ui components here //
 // Container, Paper, Chip //
 
@@ -7,14 +11,37 @@ const Car = (props) => {
     const carId = props.match.params.id
     const foundCar = cars.find(car => car.id == carId)
     console.log(foundCar)
-    // ! Console log found car and style with material ui to make look like photo in read me
-    // TODO import material ui and render the information with one of their components
+    // ! Material UI wont work even after trying ALL of these https://stackoverflow.com/questions/69429172/module-not-found-cant-resolve-mui-material-utils-v5-which-included-a-na
     return (
-        <>
-            <h1>{foundCar.Name}</h1>
+        <Card>
+            {/* <Stack direction="row" spacing={1}>
+                <Chip label={foundCar.Name} />
+                <Chip label={foundCar.Year} />
+                <Chip label={foundCar.Acceleration} />
+                <Chip label={foundCar.Miles_per_Gallon} />
+                <Chip label={foundCar.Cylinders} />
+                <Chip label={foundCar.Displacement} />
+                <Chip label={foundCar.Weight_in_lbs} />
+                
+            </Stack> */}
+
+                <Chip label={`Name: ${foundCar.Name}`} />
+                <Chip label={`Year: ${foundCar.Year}`} />
+                <Chip label={`Acceleration: ${foundCar.Acceleration}`} />
+                <Chip label={`MPG: ${foundCar.Miles_per_Gallon}`} />
+                <Chip label={`Cylinders: ${foundCar.Cylinders}`} />
+                <Chip label={`Displacement: ${foundCar.Displacement}`} />
+                <Chip label={`Weight: ${foundCar.Weight_in_lbs}`} />
+
+            {/* <h1>{foundCar.Name}</h1>
             <h3>Origin: {foundCar.Origin}</h3>
             <h3>Year: {foundCar.Year}</h3>
-        </>
+            <h3>Acceleration: {foundCar.Acceleration}</h3>
+            <h3>MPG: {foundCar.Miles_per_Gallon} </h3>
+            <h3>Cylinders: {foundCar.Cylinders} </h3>
+            <h3>Displacement: {foundCar.Displacement}</h3>
+            <h3>Weight: {foundCar.Weight_in_lbs}</h3> */}
+        </Card>
     )
 }
 
